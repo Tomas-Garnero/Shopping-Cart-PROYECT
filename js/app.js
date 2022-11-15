921273add57752a034d0d7f74113c33a40fff6f0
unknown <tomygarnero9429@gmail.com>
2022-11-15 13:20:35 -0300

    Agergando elementos al carrito

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Variables
 const carrito = document.querySelector("#carrito");
 
 const contenedorCarrito = document.querySelector("#lista-carrito tbody");
@@ -6,13 +7,17 @@ const vaciarCarritoBtn = document.querySelector("#vaciar-carrito");
 
 const listaCursos = document.querySelector("#lista-cursos");
 
+let articulosCarritos = [];
+
 
 function cargarEventListeners() {
+    // Cuando agregas un curso presionando "Agregar al Carrito"
     listaCursos.addEventListener("click", agregarCurso);
 }
 
 cargarEventListeners();
 
+// Funciones
 function agregarCurso(e) {
     e.preventDefault();
 
@@ -22,8 +27,11 @@ function agregarCurso(e) {
     }
 }
 
+// Lee el contenido del HTML al que le dimos click y extrae la info del curso
 function leerDatosCurso(curso) {
     // console.log(curso);
+
+    // Crea un objeto con el contenido del curso actual
     const infoCurso = {
         imagen: curso.querySelector("img").src,
         titulo: curso.querySelector("h4").textContent,
@@ -31,6 +39,46 @@ function leerDatosCurso(curso) {
         id: curso.querySelector("a").getAttribute("data-id"),
         cantidad: 1
     }
-    console.log(infoCurso)
+
+    // Agrega elementos al arreglo de carrito
+    articulosCarritos = [...articulosCarritos, infoCurso];
+
+    console.log(articulosCarritos);
+    carritoHTML();
 }
+
+// Muestra eñ carrito de compras en el HTML
+function carritoHTML() {
+
+    // Limpiar el HTML
+    limpiarHTML();
+
+    // Recorre el carrito y genera el HTML
+    articulosCarritos.forEach(curso => {
+        const {imagen, titulo, precio, cantidad, id} = curso;
+        const row = document.createElement("tr");
+        row.innerHTML = `
+        <td><img src="${imagen}" width="110"></td>
+        <td>${titulo}</td>
+        <td>${precio}</td>
+        <td>${cantidad}</td>
+        <td><a href="#" class="borrar-curso" data-id="${id}" > X </td>
+        `;
+
+        // Agrega el HTML del carrito en el tbody
+        contenedorCarrito.appendChild(row);
+    })
+}
+
+// Elimina los cursos del tbody
+function limpiarHTML() {
+    // Forma lenta
+    // contenedorCarrito.innerHTML= "";
+
+    while(contenedorCarrito.firstChild) {
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+    }
+}
+
+
 
